486e07401df032760a764207292c6788b4c39949
oksanaHulyaeva <hulyaevaoksana@gmail.com>
2021-04-30 18:21:29 +0300

    init: init project configs

---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack');
+
+const isDev = process.env.NODE_ENV === 'development'; // Node.js variable
+const isProd = !isDev;
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: isDev ? 'development' : 'production',
+  devtool: isDev ? 'source-map' : 'none',
+  entry: ['./js/main.js', './sass/main.scss'],
+  output: {
+    filename: `./js/${filename('js')}`,
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[name][ext][query]',
+    publicPath: ''
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, 'app'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 3000,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+      minify: {
+        collapseWhitespace: isProd
+      },
+      inject: true,
+    }),
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'src/pages/map.html'),
+      filename: 'map.html',
+      minify: {
+        collapseWhitespace: isProd
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: `./css/${filename('css')}`
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {from: path.resolve(__dirname, 'src/assets') , to: path.resolve(__dirname, 'dist/assets')}
+      ]
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev
+            },
+          },
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: (resourcePath, context) => {
+                return path.relative(path.dirname(resourcePath), context) + '/';
+              },
+            }
+          },
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.(?:|gif|png|jpg|jpeg|svg)$/,
+        type: 'asset/resource'
+      },
+    ]
+  }
+}
